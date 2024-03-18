9b1fa5c846d2235971295ddcd42e146a5c7a4fe6
gzitei <55924995+gzitei@users.noreply.github.com>
2024-03-18 15:19:51 -0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,1 @@
-require("setup")
---vim.cmd('source $HOME/.config/nvim/lua/setup/packer.lua')
---vim.cmd('cd /mnt/AdditionalDisk/coding/projects/')
---vim.g.netrw_dir = '/mnt/AdditionalDisk/coding/projects/'
---vim.g.netrw_liststyle = 3
---vim.g.netrw_browse_x = 'subl'
---vim.g.netrw_browsex_viewer = "subl"
+require("setup")-
